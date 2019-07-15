2b9e062d264daeabaa4785f4b6571dd615b27d14
Sanmathi05 <52269862+Sanmathi05@users.noreply.github.com>
2019-07-15 11:50:13 +0530

    Update psame.py

--- psame.py
+++ psame.py
@@ -1,6 +1,18 @@
-import os
-n=int(input(""))
-l=[]
-for i in range (n):
-	l.append(input())
-	print(os.path.commonprefix(l))
+y = int(input())
+lst= []
+for _ in range(y):
+    lst.append(input())
+x, c, flag = '', 0, False
+for i in lst[0]:
+    for j in lst[1:]:
+        if len(j) == c:
+            flag = True
+            break
+        if j[c] != i:
+            break
+    else:
+        x+= i
+    if flag:
+        break
+    c += 1
+print(x)

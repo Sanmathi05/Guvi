bb7bdba27e92bd0d55b6083fbbdce92fea3e8168
Sanmathi05 <52269862+Sanmathi05@users.noreply.github.com>
2019-08-01 11:43:21 +0530

    Create atm2.py

---
+++ atm2.py
@@ -0,0 +1,51 @@
+name=input("username:")
+pin=input("pin:")
+
+balance=5000
+  
+data=['guvi','123']
+def withdraw():
+  amt=int(input("enter the amount:"))
+  if(amt<5000):
+        print("collect yor cash ")
+        balance=balance-amt
+        print(balance)
+  else:
+      print('insufficient balance')
+      withdraw()
+def deposit():
+    amt1=int(input("enter the amount:"))
+    balance=balance+amt1
+    print("balance:Rs.",balance)
+def balaceenq():
+  print(balance)
+if(name and pin in data):
+  print("welcome",name)
+  while True:
+   print("1.saving account \n 2.current account")
+   c=int(input())
+   if(c == 1):
+       print("1.withdraw \n 2.deposit \n 3.balance enquiry")
+   elif(c == 2):
+       print("1.withdraw \n 2.deposit \n 3.balance enquiry")
+   else:
+       print('invalid please select 1 or 2')
+   n=int(input())
+   if(n==1):
+     
+        withdraw()
+
+     
+   elif(n==2):
+         deposit()
+    
+   elif(n==3):
+         balaceenq()
+   else:
+     print("invalid choice")
+   quit = input("Do you want to continue (y/n) ?")
+   if quit == 'n':
+    break
+       
+else:
+  print("invalid username or pin")

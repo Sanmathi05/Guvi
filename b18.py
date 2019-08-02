a2fb7c73ab9fb17ef2fc93b37f74102d71430ec6
Sanmathi05 <52269862+Sanmathi05@users.noreply.github.com>
2019-08-02 14:37:18 +0530

    Create b18.py

---
+++ b18.py
@@ -0,0 +1,11 @@
+n1,n2=map(int,input().split())
+for num in range(n1, n2):
+   order = len(str(num))
+   sum = 0
+   t = num
+   while t > 0:
+    digit = t % 10
+    sum += digit ** order
+    t //= 10
+    if num == sum:
+      print(num,end' ')
